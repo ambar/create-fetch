e7f8c05a1975e1067aba137d033f159901f9aace
ambar <ambar.lee@gmail.com>
2018-12-07 12:35:02 +0800

    build: use babelrc.js

---
+++ .babelrc.js
@@ -0,0 +1,26 @@
+const create = env => [
+  [
+    '@babel/preset-env',
+    {
+      loose: true,
+      // use native `Object.assign`
+      useBuiltIns: 'entry',
+      ...env,
+    },
+  ],
+]
+
+module.exports = {
+  presets: create(),
+  env: {
+    es: {
+      presets: create({modules: false}),
+    },
+    test: {
+      presets: create({targets: {node: 'current'}}),
+      plugins: [
+        ['@babel/plugin-proposal-pipeline-operator', {proposal: 'minimal'}],
+      ],
+    },
+  },
+}
