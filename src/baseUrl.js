fbc6bb73a0379659b88aaaf52d80bcf47535fe56
ambar <ambar.lee@gmail.com>
2021-08-02 11:41:40 +0800

    feat: use URL in `baseUrl()`, remove path joining

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,12 +1,2 @@
-const reAbsoluteUrl = /^(\w+:)?\/\//
-const reDataUrl = /^data:[^,]*,/
-
-const joinUrl = (a, b) => [a.replace(/\/$/, ''), b.replace(/^\//, '')].join('/')
-
-export default baseUrl => fetch => (url, options) =>
-  fetch(
-    reAbsoluteUrl.test(url) || reDataUrl.test(url)
-      ? url
-      : joinUrl(baseUrl, url),
-    options
-  )
+export default (baseUrl) => (fetch) => (url, options) =>
+  fetch(new URL(url, baseUrl).toString(), options)
--- test/baseUrl.spec.js
+++ test/baseUrl.spec.js
@@ -1,63 +1,45 @@
 import baseUrl from '../src/baseUrl'
 
 describe('baseUrl', () => {
-  const fetch = (...args) => Promise.resolve(args)
+  const fetch = async (...args) => args
+  const fetchUrls = (myFetch, urls) =>
+    Promise.all(urls.map((url) => myFetch(url)))
 
   it('should add base url', async () => {
     const myFetch = baseUrl('https://api.example.com')(fetch)
-    const result = await myFetch('/users')
-    expect(result).toContainEqual('https://api.example.com/users')
-    expect(result).toEqual(await myFetch('users'))
-
-    const myFetchWithTrailingSlash = baseUrl('https://api.example.com/')(fetch)
-    expect(result).toEqual(await myFetchWithTrailingSlash('/users'))
-    expect(result).toEqual(await myFetchWithTrailingSlash('users'))
+    const urls = ['/path?foo', 'path?foo']
+    expect(await fetchUrls(myFetch, urls)).toMatchInlineSnapshot(`
+Array [
+  Array [
+    "https://api.example.com/path?foo",
+    undefined,
+  ],
+  Array [
+    "https://api.example.com/path?foo",
+    undefined,
+  ],
+]
+`)
   })
 
-  it('should add base path', async () => {
-    const myFetch = baseUrl('/apis/v4')(fetch)
-    const result = await myFetch('/users')
-    expect(result).toContainEqual('/apis/v4/users')
-    expect(result).toEqual(await myFetch('users'))
-  })
-
-  it('should not add base url for absolute url', async () => {
-    const myFetch = baseUrl('//api.example.com')(fetch)
-    const absoluteUrls = [
-      '//api2.example.com',
-      'http://api2.example.com',
-      'https://api2.example.com',
-      'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=',
-    ]
-    for (const url of absoluteUrls) {
-      expect(await myFetch(url)).toContainEqual(url)
-    }
-
-    const nonAbsoluteUrls = ['/http://example.com', '/http://example.com']
-    for (const url of nonAbsoluteUrls) {
-      expect(await myFetch(url)).not.toContainEqual(url)
-    }
-  })
-
-  it('should not add base url for data url', async () => {
-    const myFetch = baseUrl('//api.example.com')(fetch)
-    const dataUrls = [
-      'data:text/*,Hi',
-      'data:text/plain,Hi',
-      'data: text/plain,Hi',
-      'data:text/plain;charset=US-ASCII,Hi',
-      'data:text/plain;base64,aGk=',
-      'data:,Hi',
-      'data: , Hi',
-      'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=',
-    ]
-    for (const url of dataUrls) {
-      expect(await myFetch(url)).toContainEqual(url)
-    }
-
-    const nonDataUrls = ['data:hi', '/data:text/*,Hi']
-    for (const url of nonDataUrls) {
-      expect(await myFetch(url)).not.toContainEqual(url)
-    }
+  it('should not change non-http urls', async () => {
+    const myFetch = baseUrl('https://api.example.com')(fetch)
+    const urls = ['ws://example.com', 'blob:http://localhost/uuid', 'data:,']
+    expect(await fetchUrls(myFetch, urls)).toMatchInlineSnapshot(`
+Array [
+  Array [
+    "ws://example.com/",
+    undefined,
+  ],
+  Array [
+    "blob:http://localhost/uuid",
+    undefined,
+  ],
+  Array [
+    "data:,",
+    undefined,
+  ],
+]
+`)
   })
 })
